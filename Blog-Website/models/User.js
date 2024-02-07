3fc08ff9645fbabe01a6af727ce53d34314b737d
Perabathula Maa Renuka <131272572+maarenuka@users.noreply.github.com>
2024-02-08 01:18:24 +0530

    Create User.js

---
+++ Blog-Website/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: "String",
+      required: true,
+    },
+    email: {
+      type: "String",
+      required: true,
+    },
+    password: {
+      type: "String",
+      required: true,
+    },
+    refreshToken: {
+      type: "String",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
